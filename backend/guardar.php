e8ae1bda68ef5538a77b0be767fc5515d77f22bd
jacarrascal <jacarrascal@educa.jcyl.es>
2026-03-09 19:42:37 +0100

    Update guardar.php

--- backend/guardar.php
+++ backend/guardar.php
@@ -1,6 +1,17 @@
 <?php
 $conn = new mysqli("postgresql://root:lRiVcodA9HuuAZJX7fTI2RajV300GnOv@dpg-d6nh5ghaae7s73avs7dg-a.oregon-postgres.render.com/oceano_sostenible");
+// Extraer componentes de la URL
+$url = parse_url($db_url);
 
+$host = $url["host"];
+$port = $url["port"];
+$user = $url["user"];
+$pass = $url["pass"];
+$dbname = ltrim($url["path"], "/");
+
+$dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+$pdo = new PDO($dsn, $user, $pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    
 $id_ub = $_POST['id_ubicacion'];
 $temp = $_POST['temp'];
 $ph = $_POST['ph'];
